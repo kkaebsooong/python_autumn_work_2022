ea1b398da73a3536cab00c357ef827f4018c1ee6
kkaebsooong <sgaatlantis@inbox.ru>
2022-09-18 13:38:33 +0300

    Добавлены дз к lesson_2

---
+++ homework/task4.py
@@ -0,0 +1,9 @@
+# todo:  Создайте переменные, что бы вывести требуемый текст.
+
+# задан нерабочий код
+# требуемый вывод:
+# Мой стек: python, javascript, php
+programming_lang_1 = 'python'
+programming_lang_2 = 'javascript'
+programming_lang_3 = 'php'
+print(f'Мой стек: {programming_lang_1}, {programming_lang_2}, {programming_lang_3}')
