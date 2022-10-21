110d26e3541f9eb464df0415f527518eac43eb2c
kkaebsooong <sgaatlantis@inbox.ru>
2022-10-21 11:22:40 +0300

    добавлена задача task24.py

--- unit_one/task24.py
+++ unit_one/task24.py
@@ -9,78 +9,17 @@
 
 
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'g', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-            'v', 'w', 'z', 'y', 'z']
-decryption = ''
-numbers = input()
-step = -1
+alphabet = ' abcdefghigklmnopqrstuvwxyz,!'
+encrypted_text = (8, 5, 12, 12, 15, ',', 0, 23, 15, 18, 12, 4, '!')
+new_text =[]
 
-for i in numbers:
-    index = numbers.find(i)
-    new_index = index + step
-    if i in numbers:
-        decryption += numbers[new_index]
-    else:
-        decryption += i
-print(decryption)
+for i in encrypted_text[:encrypted_text.index(',')]:
+    new_text.append(alphabet[i])
+for j in encrypted_text[encrypted_text.index(',') + 1:encrypted_text.index('!')]:
+    new_text.append(alphabet[j])
 
-# def decryption(i):
-#     match i:
-#         case 1:
-#             print('a')
-#         case 2:
-#             print('b')
-#         case 3:
-#             print('c')
-#         case 4:
-#             print('d')
-#         case 5:
-#             print('e')
-#         case 6:
-#             print('f')
-#         case 7:
-#             print('g')
-#         case 8:
-#             print('h')
-#         case 9:
-#             print('i')
-#         case 10:
-#             print('j')
-#         case 11:
-#             print('k')
-#         case 12:
-#             print('l')
-#         case 13:
-#             print('m')
-#         case 14:
-#             print('n')
-#         case 15:
-#             print('o')
-#         case 16:
-#             print('p')
-#         case 17:
-#             print('q')
-#         case 18:
-#             print('r')
-#         case 19:
-#             print('s')
-#         case 20:
-#             print('t')
-#         case 21:
-#             print('u')
-#         case 22:
-#             print('v')
-#         case 23:
-#             print('w')
-#         case 24:
-#             print('x')
-#         case 25:
-#             print('y')
-#         case 26:
-#             print('z')
-#
-# decryption(8)
-# decryption(5)
-# decryption(12)
-# decryption(12)
-# decryption(15)
+new_text = ''.join(new_text)
+new_text = new_text.rstrip(' ')
+new_text = new_text.replace(' ', ', ')
+
+print(f'{new_text}!')
